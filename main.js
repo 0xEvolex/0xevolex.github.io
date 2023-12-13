b377bba007e289d04d996518abed68805e5a0753
0xEvolex <kroll_business@gmx.de>
2023-12-13 23:18:10 +0100

    idk what im doing

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
-// main.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Fetch events data from events.json
-    fetch('/events.json')
-      .then(response => response.json())
-      .then(eventsData => {
-        // Call the function to display remaining time
-        displayRemainingTime(eventsData);
-      })
-      .catch(error => console.error('Error fetching events data:', error));
-  
-    // Function to calculate and display remaining time
-    function displayRemainingTime(eventsData) {
-      // Get the current date and time
-      const currentDate = new Date();
-  
-      // Loop through each event type
-      Object.keys(eventsData).forEach(eventType => {
-        // Loop through each day for the event type
-        Object.keys(eventsData[eventType]).forEach(day => {
-          // Loop through each event time for the day
-          eventsData[eventType][day].forEach(eventTime => {
-            // Parse event time string to create a Date object
-            const [hours, minutes] = eventTime.split(':');
-            const eventDate = new Date(currentDate);
-            eventDate.setHours(parseInt(hours, 10));
-            eventDate.setMinutes(parseInt(minutes, 10));
-            eventDate.setSeconds(0);
-  
-            // Check if the event time is in the future
-            if (eventDate > currentDate) {
-              // Calculate remaining time
-              const remainingTime = eventDate - currentDate;
-  
-              // Display remaining time (you can adjust this based on your needs)
-              const remainingHours = Math.floor(remainingTime / (1000 * 60 * 60));
-              const remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-  
-              // Display the remaining time in the console (you can update this to display on your webpage)
-              console.log(`Next ${eventType} on ${day} at ${eventTime}: ${remainingHours} hours and ${remainingMinutes} minutes remaining.`);
-            }
-          });
-        });
-      });
+// Switch to a tab
+function switchTab(tabId) {
+    // Hide all tab contents
+    const tabContents = document.getElementsByClassName('tab-content');
+    for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].style.display = 'none';
     }
-  });
-  
+
+    // Show the selected tab content
+    document.getElementById(tabId).style.display = 'block';
+}
+
+// Fetch the events data from events.json
+fetch('events.json')
+    .then(response => response.json())
+    .then(events => {
+        // Get the #events-container div
+        const eventsContainer = document.getElementById('events-container');
+
+        // For each event...
+        for (const eventName in events) {
+            const eventTimes = events[eventName];
+
+            // For each day of the week...
+            for (const dayOfWeek in eventTimes) {
+                const times = eventTimes[dayOfWeek];
+
+                // For each time...
+                times.forEach(time => {
+                    // Create a new div for the event
+                    const eventDiv = document.createElement('div');
+                    eventDiv.className = 'event';
+
+                    // Create a new div for the event name
+                    const eventNameDiv = document.createElement('div');
+                    eventNameDiv.className = 'event-name';
+                    eventNameDiv.textContent = `${eventName} - ${dayOfWeek} - ${time}`;
+                    eventDiv.appendChild(eventNameDiv);
+
+                    // Add the event div to the #events-container div
+                    eventsContainer.appendChild(eventDiv);
+                });
+            }
+        }
+    });
+
+// Switch to the "Main" tab by default
+switchTab('main');
