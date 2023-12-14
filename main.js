2e7bccdd214aa6b1304513c64a7615522627321f
0xEvolex <kroll_business@gmx.de>
2023-12-14 15:36:20 +0100

    app tab

--- main.js
+++ main.js
@@ -98,9 +98,48 @@ function switchTab(tabId) {
             userDtDiv.textContent = `User Time: ${userTime}, ${userDay}`;
         }, 1000);
     }
+
+    if (tabId === 'app') {
+        const appDiv = document.getElementById('app');
+    
+        // Clear previous content
+        appDiv.innerHTML = '';
+    
+        const disclaimerDiv = document.createElement('div');
+        disclaimerDiv.textContent = 'An app that will display you all Sailor Events and allow you to set sound alarms for them!';
+        disclaimerDiv.style.textAlign = 'center';
+        disclaimerDiv.className = 'schedule-disclaimer';  // Add the class of the disclaimer in the 'schedule' tab
+        appDiv.appendChild(disclaimerDiv);
+    
+        // Add the images
+        const imageContainer = document.createElement('div');
+        imageContainer.style.display = 'flex';
+        imageContainer.style.justifyContent = 'center';
+        appDiv.appendChild(imageContainer);
+
+        const img1 = document.createElement('img');
+        img1.src = 'images/app_preview_1.png';
+        img1.style.borderRadius = '10px';
+        img1.style.margin = '10px';
+        imageContainer.appendChild(img1);  // append to imageContainer
+
+        const img2 = document.createElement('img');
+        img2.src = 'images/app_preview_2.png';
+        img2.style.borderRadius = '10px';
+        img2.style.margin = '10px';
+        imageContainer.appendChild(img2);  // append to imageContainer
+
+        // Add the download link as a button
+        const downloadLink = document.createElement('a');
+        downloadLink.href = 'downloads/sailor_events.rar';
+        downloadLink.download = 'sailor_events.rar';
+        downloadLink.textContent = 'Download';
+        downloadLink.className = 'download-button';
+        appDiv.appendChild(downloadLink);
+    }
 }
 
-fetch('events.json')
+fetch('data/events.json')
     .then(response => response.json())
     .then(events => {
         // Get the #events-container div
